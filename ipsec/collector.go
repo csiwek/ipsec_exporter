9396b9ecd72be812b5f09a599ea2c5c912734a01
Cezary Siwek <cezary.siwek@gmail.com>
2020-12-07 22:21:52 +0100

    debug

--- ipsec/collector.go
+++ ipsec/collector.go
@@ -1,20 +1,20 @@
 package ipsec
 
 import (
+	"fmt"
 	"github.com/prometheus/client_golang/prometheus"
-//	"fmt"
 )
 
 var (
-	metricUp = prometheus.NewDesc("ipsec_up", "value indicating a successful scrape", []string{"tunnel","user"}, nil)
-  metricStatus = prometheus.NewDesc("ipsec_tunnel_status", "ipsec status value", []string{"tunnel","user"}, nil)
-	metricBytesIn = prometheus.NewDesc("ipsec_in_bytes", "received bytes per tunnel", []string{"tunnel","user"}, nil)
-	metricBytesOut = prometheus.NewDesc("ipsec_out_bytes", "sent bytes per tunnel", []string{"tunnel","user"}, nil)
-	metricPacketsIn = prometheus.NewDesc("ipsec_in_packets", "received packets per tunnel", []string{"tunnel","user"}, nil)
-	metricPacketsOut = prometheus.NewDesc("ipsec_out_packets", "sent packets per tunnel", []string{"tunnel","user"}, nil)
+	metricUp         = prometheus.NewDesc("ipsec_up", "value indicating a successful scrape", []string{"tunnel", "user"}, nil)
+	metricStatus     = prometheus.NewDesc("ipsec_tunnel_status", "ipsec status value", []string{"tunnel", "user"}, nil)
+	metricBytesIn    = prometheus.NewDesc("ipsec_in_bytes", "received bytes per tunnel", []string{"tunnel", "user"}, nil)
+	metricBytesOut   = prometheus.NewDesc("ipsec_out_bytes", "sent bytes per tunnel", []string{"tunnel", "user"}, nil)
+	metricPacketsIn  = prometheus.NewDesc("ipsec_in_packets", "received packets per tunnel", []string{"tunnel", "user"}, nil)
+	metricPacketsOut = prometheus.NewDesc("ipsec_out_packets", "sent packets per tunnel", []string{"tunnel", "user"}, nil)
 )
 
-func NewCollector(configurations ... *Configuration) *Collector {
+func NewCollector(configurations ...*Configuration) *Collector {
 	return &Collector{
 		configurations: configurations,
 	}
@@ -35,12 +35,12 @@ func (c *Collector) Describe(ch chan<- *prometheus.Desc) {
 
 func (c *Collector) Collect(ch chan<- prometheus.Metric) {
 	for _, configuration := range c.configurations {
-		for _, tunnelconfig :=  range configuration.tunnel {
-			if ((tunnelconfig.auth == "eap") || (tunnelconfig.auth == "xauth")) {
+		for _, tunnelconfig := range configuration.tunnel {
+			if (tunnelconfig.auth == "eap") || (tunnelconfig.auth == "xauth") {
 				status := queryUserStatus(tunnelconfig, &cliStatusProvider{})
 				for tunnel, tunnelStatus := range status {
 					for user, userdata := range tunnelStatus {
-//					fmt.Println("tunnel name: ", tunnel, "\nStatus: ", userdata,"\n")
+						fmt.Println("tunnel name: ", tunnel, "\nStatus: ", userdata, "\n")
 						ch <- prometheus.MustNewConstMetric(metricUp, prometheus.GaugeValue, c.toFloat64(userdata.up), tunnel, user)
 						ch <- prometheus.MustNewConstMetric(metricStatus, prometheus.GaugeValue, float64(userdata.status), tunnel, user)
 						ch <- prometheus.MustNewConstMetric(metricBytesIn, prometheus.CounterValue, float64(userdata.ubytesIn), tunnel, user)
@@ -50,17 +50,17 @@ func (c *Collector) Collect(ch chan<- prometheus.Metric) {
 					}
 				}
 			} else {
-	        status := queryTunnelStatus(tunnelconfig, &cliStatusProvider{})
-					user := ""
-					for tunnel, tunnelStatus := range status {
-//						fmt.Println("tunnel name: ", tunnel, "\nStatus: ", tunnelStatus,"\n")
-                	                	ch <- prometheus.MustNewConstMetric(metricUp, prometheus.GaugeValue, c.toFloat64(tunnelStatus.up), tunnel, user)
-                        	        	ch <- prometheus.MustNewConstMetric(metricStatus, prometheus.GaugeValue, float64(tunnelStatus.status), tunnel, user)
-	                                	ch <- prometheus.MustNewConstMetric(metricBytesIn, prometheus.CounterValue, float64(tunnelStatus.bytesIn), tunnel, user)
-       		                        	ch <- prometheus.MustNewConstMetric(metricBytesOut, prometheus.CounterValue, float64(tunnelStatus.bytesOut), tunnel, user)
-                	                	ch <- prometheus.MustNewConstMetric(metricPacketsIn, prometheus.CounterValue, float64(tunnelStatus.packetsIn), tunnel, user)
-                        	        	ch <- prometheus.MustNewConstMetric(metricPacketsOut, prometheus.CounterValue, float64(tunnelStatus.packetsOut), tunnel, user)
-          }
+				status := queryTunnelStatus(tunnelconfig, &cliStatusProvider{})
+				user := ""
+				for tunnel, tunnelStatus := range status {
+					fmt.Println("tunnel name: ", tunnel, "\nStatus: ", tunnelStatus, "\n")
+					ch <- prometheus.MustNewConstMetric(metricUp, prometheus.GaugeValue, c.toFloat64(tunnelStatus.up), tunnel, user)
+					ch <- prometheus.MustNewConstMetric(metricStatus, prometheus.GaugeValue, float64(tunnelStatus.status), tunnel, user)
+					ch <- prometheus.MustNewConstMetric(metricBytesIn, prometheus.CounterValue, float64(tunnelStatus.bytesIn), tunnel, user)
+					ch <- prometheus.MustNewConstMetric(metricBytesOut, prometheus.CounterValue, float64(tunnelStatus.bytesOut), tunnel, user)
+					ch <- prometheus.MustNewConstMetric(metricPacketsIn, prometheus.CounterValue, float64(tunnelStatus.packetsIn), tunnel, user)
+					ch <- prometheus.MustNewConstMetric(metricPacketsOut, prometheus.CounterValue, float64(tunnelStatus.packetsOut), tunnel, user)
+				}
 			}
 		}
 	}
